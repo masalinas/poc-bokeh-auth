c9290ef66ac0bb16244d0a1a81f75f01951a27fe
Miguel Angel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-11-02 10:18:02 +0100

    last commit

--- bootstrat.py
+++ bootstrat.py
@@ -3,8 +3,10 @@ from bokeh.server.auth_provider import AuthModule
 
 from bokeh.command.util import build_single_handler_application
 
+# Define Bokeh Prefix Web Context
 prefix = "MP"
 
+# Define Bokeh Authentication Module
 auth_provider = AuthModule('./auth/auth.py')
 
 # Define Bokeh application
@@ -19,5 +21,6 @@ bokeh_server = Server(
 auth_provider._module.login_url = f'/{prefix}/login/'
 auth_provider._module.logout_url = f'/{prefix}/logout/'
 
+# Start Bokeh Server
 bokeh_server.start()
 bokeh_server.io_loop.start()
